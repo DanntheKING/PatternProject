7d78b959d1f0d77a1069771e0388d0b9a759cbe4
DanntheKING <Danielwinston669485@gmail.com>
2022-10-14 17:53:17 -0500

    Added Build pattern C# code

--- source/Builder.cs
+++ source/Builder.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class RoomBuilder
+    {
+        private string subject;
+        private int roomNum;
+        private string instructor;
+
+        public RoomBuilder WithSubject(string Subject)
+        {
+            subject = Subject;
+            return this;
+        }
+
+        public RoomBuilder WithRoomNum(int RoomNum)
+        {
+            roomNum = RoomNum;
+            return this;
+        }
+
+        public RoomBuilder WithInstructor(string Instructor)
+        {
+            instructor = Instructor;
+            return this;
+        }
+
+        public RoomBuilder Build()
+        {
+            return this;
+        }
+    }
+}
