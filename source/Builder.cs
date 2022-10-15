223556d1910ffdcb095bc0dd0a21b803783ec889
DanntheKING <Danielwinston669485@gmail.com>
2022-10-14 19:32:58 -0500

    Added comments to my code

--- source/Builder.cs
+++ source/Builder.cs
@@ -9,29 +9,34 @@ using System.Threading.Tasks;
 namespace Builder
 {
     public class RoomBuilder
-    {
+    {   
+        // Creates the private fields needed to create  a room
         private string subject;
         private int roomNum;
         private string instructor;
 
+        // This method is used to set and change the subject attribute of the room
         public RoomBuilder WithSubject(string Subject)
         {
             subject = Subject;
             return this;
         }
 
+        // This method is used to set and change the room number attribute of the room
         public RoomBuilder WithRoomNum(int RoomNum)
         {
             roomNum = RoomNum;
             return this;
         }
 
+        // This method is used to set and change the instructor attribute of the room
         public RoomBuilder WithInstructor(string Instructor)
         {
             instructor = Instructor;
             return this;
         }
 
+        // This is the main build method that returns all the requirements
         public RoomBuilder Build()
         {
             return this;
