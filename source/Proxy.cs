df9c264f240acca3ce47c43be1626e079dae7ad1
Amiyah <51186075+amiyahfrierson@users.noreply.github.com>
2022-10-23 21:30:15 -0500

    Add Proxy.cs

---
+++ source/Proxy.cs
@@ -0,0 +1,54 @@
+﻿//Amiyah Frierson
+//CSC 403-002
+//24 October 2022
+
+using System;
+namespace ProxyPattern
+{
+    public class Boarding
+    {
+        public static void Main(string[] args)
+        {
+            // Create math proxy
+            BagCheckArea securityProxy = new BagCheckArea();
+
+            // Let the Passengers board
+            securityProxy.CheckBags("contraband!", "nike shoes");
+            securityProxy.CheckBags("gel pillow", "lipstick");
+            securityProxy.CheckBags("lucky keychain", "contraband!");
+            securityProxy.CheckBags("hair brush", "ceramic mug");
+        }
+    }
+
+    /// The Subject Class
+    public interface IDetector
+    {
+        void CheckBags(string x, string y);
+    }
+
+    /// The Real_Subject Class
+    public class Security : IDetector
+    {
+        public void CheckBags(string item1, string item2) 
+        {
+            if ((String.Equals(item1, "contraband!")) || (String.Equals(item2, "contraband!")))
+            {
+                Console.WriteLine("The passenger with contraband was detained!");
+            }
+            else
+            {
+                Console.WriteLine("The passenger boarded the plane without issue.");
+            }
+        }
+    }
+
+    /// The Protection Proxy Class 
+    public class BagCheckArea : IDetector
+    {
+        private Security guard = new Security();
+        public void CheckBags(string item1, string item2)
+        {
+            guard.CheckBags(item1, item2);
+        }
+    }
+}
